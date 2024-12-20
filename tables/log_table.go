d2917c18462e287e295c89f9fddcfca255eeb15e
kai <kai@turbot.com>
2024-12-20 11:01:07 +0000

    move CommonFields to schema

--- tables/log_table.go
+++ tables/log_table.go
@@ -4,9 +4,9 @@ import (
 	"github.com/turbot/tailpipe-plugin-custom/formats"
 	"github.com/turbot/tailpipe-plugin-sdk/artifact_source"
 	"github.com/turbot/tailpipe-plugin-sdk/constants"
-	"github.com/turbot/tailpipe-plugin-sdk/enrichment"
 	"github.com/turbot/tailpipe-plugin-sdk/mappers"
 	"github.com/turbot/tailpipe-plugin-sdk/row_source"
+	"github.com/turbot/tailpipe-plugin-sdk/schema"
 	"github.com/turbot/tailpipe-plugin-sdk/table"
 )
 
@@ -33,7 +33,7 @@ func (c *LogTable) Identifier() string {
 	return c.Name
 }
 
-func (c *LogTable) EnrichRow(row *table.DynamicRow, sourceEnrichmentFields enrichment.SourceEnrichment) (*table.DynamicRow, error) {
+func (c *LogTable) EnrichRow(row *table.DynamicRow, sourceEnrichmentFields schema.SourceEnrichment) (*table.DynamicRow, error) {
 	// tell the row to enrich itself using any mappings specified in the source format
 	row.Enrich(sourceEnrichmentFields.CommonFields)
 	return row, nil
