2219bc07d37ead1162d63567892f386722ea9b50
kaidaguerre <kai@turbot.com>
2025-03-21 12:35:09 +0000

    update ref (#10)

--- core/get_sources.go
+++ core/get_sources.go
@@ -1,9 +1,12 @@
 package core
 
-import "github.com/turbot/tailpipe-plugin-sdk/row_source"
+import (
+	"github.com/turbot/tailpipe-plugin-sdk/row_source"
+	"github.com/turbot/tailpipe-plugin-sdk/types"
+)
 
 // DescribeSources returns a map of source metadata - it is called by the CLI to determine which sources are
 // provided by the core plugin
-func DescribeSources() (row_source.SourceMetadataMap, error) {
+func DescribeSources() (types.SourceMetadataMap, error) {
 	return row_source.Factory.DescribeSources()
 }
